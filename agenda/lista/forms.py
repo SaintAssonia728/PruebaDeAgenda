1d6953a99bdd20c088ca2d2ab34793ae775bb18a
SaintAssonia728 <mauricio.gomez33@inacapmail.cl>
2025-09-29 13:47:52 -0300

    se modifico

--- agenda/lista/forms.py
+++ agenda/lista/forms.py
@@ -5,3 +5,4 @@ class ContactosForm(forms.ModelForm):
     class Meta:
         model = Contactos
         fields = ['nombre', 'correo', 'telefono', 'direccion'] # Colocamos estos campos los cuales son los que solicita la agenda de contactos
+        
--- agenda/lista/urls.py
+++ agenda/lista/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("contacto/nuevo/", views.nuevo_contactos, name="nuevo_contactos"),
     path("contacto/editar/<int:id>/", views.editar_contactos, name="editar_contactos"),
     path("contacto/eliminar/<int:id>/", views.eliminar_contactos, name="eliminar_contactos"),
-    path("contacto/buscar/", views.buscar_contactos, name="buscar_contactos"),
 ]
 # Le cambiamos el nombre a la vista por el que vamos a llamar en este caso Contactos y le damos un nombre a la url para identificarla mejor en la carpeta templates
