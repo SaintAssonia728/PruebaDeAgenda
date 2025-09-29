691ee819e71b803bf822aa9d57c113d73809ccce
Mauricio Ignacio Gomez Gonzalez <mauricio.gomez33@inacapmail.cl>
2025-09-29 17:16:05 -0300

    Mas Comentarios

--- agenda/lista/models.py
+++ agenda/lista/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Contactos(models.Model):
-    nombre = models.CharField(max_length=100)
-    correo = models.EmailField(unique=True) # Usamos EmailField para validar que el correo tenga un formato correcto y unico
+    nombre = models.CharField(max_length=100) 
+    correo = models.EmailField(unique=True) # Usamos EmailField para validar que el correo tenga un formato correcto y unico al usarlo genera mensajes automaticamenta por django el cual lo podemos cambiar al usar con (mensaje_error)
     telefono = models.CharField(max_length=15)
     direccion = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre
+    
+    #Para El Nombre, Telefono, Direccion usaremos (Charfield) ya que con este tipo de modelo o dato en djangio para ekl nombre es perfecto sobre todo por la cantidad que se almcena 
