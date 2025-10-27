c4b12668fa24c44698b26cc39a23b60d6a7d3741
SaintAssonia728 <mauricio.gomez33@inacapmail.cl>
2025-10-27 16:00:59 -0300

    Revert "agregamos"
    This reverts commit 4aeefd913304c8895a2034c194828a089aa1c5f7.

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import environ
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,12 +97,8 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DBNAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
