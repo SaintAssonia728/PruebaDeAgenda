0dd4c6145f8ae73412a2859778e65977ebe80bfa
SaintAssonia728 <mauricio.gomez33@inacapmail.cl>
2025-12-14 16:23:32 -0300

    se cambio a antes

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -1,28 +1,22 @@
 """
 URL configuration for agenda project.
-...
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
 from django.contrib import admin
-
-# --- AÑADIR ESTOS IMPORTS ---
-from django.conf import settings
-from django.conf.urls.static import static
-import os
-# -----------------------------
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("lista.urls")),  # Añadimos las urls de la app lista
-] 
-
-# ------------------------------------------------------------------
-# **BLOQUE PARA SERVIR ARCHIVOS ESTÁTICOS EN DESARROLLO**
-# Esto es esencial si Django no logra encontrar la carpeta 'static'
-# automáticamente y corrige el error 404 para /static/chat.html
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=os.path.join(settings.BASE_DIR, 'static')
-    )
-# ------------------------------------------------------------------
+    path("", include("lista.urls")),  # Añadimos las urls de la app lista para que las reconozca en la agenda así de tal manera que podamos utilizarlas
+]
